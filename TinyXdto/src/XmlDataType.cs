b30750d90a711473988523f69af4a06a52a1de0d
Сергѣй Батанов <dmpas@dmpas.ru>
2016-08-13 00:05:33 +0300

    Добавлены классы XmlDataType и XmlExpandedName

--- TinyXdto/src/XmlDataType.cs
+++ TinyXdto/src/XmlDataType.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
 namespace TinyXdto
 {
-	public class XmlDataType
+	[ContextClass("ТипДанныхXML", "XMLDataType")]
+	public class XmlDataType : AutoContext<XmlDataType>
 	{
-		public XmlDataType ()
+		internal XmlDataType (string typeName, string namespaceUri)
 		{
+			NamespaceUri = namespaceUri;
+			TypeName = typeName;
+		}
+
+		[ContextProperty("URIПространстваИмен", "NamespaceURI")]
+		public string NamespaceUri { get; }
+
+		[ContextProperty("ИмяТипа", "TypeName")]
+		public string TypeName { get; }
+
+		[ScriptConstructor]
+		public static IReflectableContext Constructor (IValue typeName, IValue namespaceUri)
+		{
+			return new XmlDataType (typeName.ToString(), namespaceUri.ToString());
 		}
 	}
 }
--- TinyXdto/src/XmlExpandedName.cs
+++ TinyXdto/src/XmlExpandedName.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.Machine;
+
 namespace TinyXdto
 {
-	public class XmlExpandedName
+	[ContextClass("РасширенноеИмяXML", "XMLExpandedName")]
+	public class XmlExpandedName : AutoContext<XmlExpandedName>
 	{
-		public XmlExpandedName ()
+		internal XmlExpandedName (string namespaceUri, string localName)
 		{
+		}
+
+		[ContextProperty("URIПространстваИмен", "NamespaceURI")]
+		public string NamespaceUri { get; }
+
+		[ContextProperty("ЛокальноеИмя", "LocalName")]
+		public string LocalName { get; }
+
+		[ScriptConstructor]
+		public static IReflectableContext Constructor (IValue namespaceUri, IValue localName)
+		{
+			return new XmlExpandedName (namespaceUri.ToString (), localName.ToString ());
 		}
 	}
 }
