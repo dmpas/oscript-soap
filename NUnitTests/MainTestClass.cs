2b556c8fc19071f07c192a8b66c466f591c2b267
Sergey Batanov <sergey.batanov@dmpas.ru>
2016-12-30 13:21:14 +0300

    Добавлено тестирование.
     * NUint-проект
     * Настройка для travis-ci
     * Настройка для appveyor

---
+++ NUnitTests/MainTestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ScriptEngine.HostedScript;
+using ScriptEngine.Machine;
+using ScriptEngine.Environment;
+using TinyXdto;
+using OneScript.Soap;
+
+// Используется NUnit 3.6
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class MainTestClass
+    {
+
+        private EngineHelpWrapper host;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            host = new EngineHelpWrapper();
+            host.StartEngine();
+        }
+
+        [Test]
+        public void TestAsInternalObjects()
+        {
+        }
+
+        [Test]
+        public void TestAsExternalObjects()
+        {
+            host.RunTestScript("NUnitTests.Tests.external.os");
+        }
+    }
+}
